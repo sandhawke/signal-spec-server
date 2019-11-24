0e828480c8e1c778073d716d6ccef7bee0c7846e
Sandro Hawke <sandro@hawke.org>
2019-11-23 21:30:09 -0500

    coalesced definitions when text matches

--- ft3.js
+++ ft3.js
@@ -66,95 +66,92 @@ async function gendoc (config) {
 function defsTable (s) {
   const out = []
 
-  const myDefs = []
+  const defTexts = new Set()
   for (const def of defs) {
-    if (def.label === s.title) myDefs.push(def)
+    if (def.label === s.title) defTexts.add(def.description)
   }
   
-  if (myDefs.length) {
-    out.push('<table>')
-    out.push('  <thead>')
-    out.push('    <tr>')
-    out.push(H`      <th>Ref</th>`)
-    out.push(H`      <th>Definition</th>`)
-    out.push('    </tr>')
-    out.push('  </thead>')
-    out.push('  <tbody>')
+  if (defTexts.size === 0) return []
+  
+  out.push('<table>')
+  out.push('  <thead>')
+  out.push('    <tr>')
+  out.push(H`      <th>Ref</th>`)
+  out.push(H`      <th>Definition</th>`)
+  out.push('    </tr>')
+  out.push('  </thead>')
+  out.push('  <tbody>')
 
-    /*
-      const tags = {} // tags[deftext][tagname] = [src1link, src2link]
-      const comments = {} // comments[deftext] = [comment1, comment2, .. ]
-      for (const def of s.defs) {
-      def.signalTitle = s.title
-      allDefs.push(def)
-      if (!tags[def.text]) tags[def.text] = {}
-      for (const tagentry of def.tags || []) {
-      if (!tags[def.text][tagentry.name]) tags[def.text][tagentry.name] = []
-      tags[def.text][tagentry.name].push(tagentry.link)
-      // didStuff = true
-      }
-      if (def.comments) {
-      if (!comments[def.text]) comments[def.text] = []
-      comments[def.text].push(def.comments)
-      }
-      }
-      // if (didStuff) console.error('TAGS', tags)
-      */
-    for (const def of myDefs) {
-      out.push('    <tr>')
-      const k = pkey.hash16(def.description)
-      out.push(H`      <td id="${k}"><a href="#${k}" style="font-size: 75%">${k}<a></td>`)
-      out.push(H`      <td style="white-space: pre-line">${def.description}</td>`)
-      /*
-        const tt = [] // same as "out", but we join with no space
-        for (const tagname of Object.keys(tags[text]).sort()) {
-        tt.push(H`${tagname}(`)
-        let links = tags[text][tagname]
-        if (links.length > 3) {
-        links = links.slice(0, 3).push('...')
-        }
-        tt.push(links.join(','))
-        tt.push(') ')
-        }
-      */
-      out.push('    </tr>')
-      
-      const comm = []
-
-      for (const entry of defs) {
-        if (entry.description !== def.description) continue
-        
-        if (entry.source) {
-          comm.push(`<p><i>Source:</i> ${entry.source}`)
-          if (entry.index && entry.id) {
-            comm.push(` via <a href="https://api.newsq.net/signals/${entry.index}/documentation">NewsQ (${entry.id}</a>)`)
-          }
-          comm.push(`</p>`)
-        }
-        
-        if (entry.demoOnly) {
-          comm.push(`<p><i>For Demonstration Purposes Only</i>.`)
-          comm.push(`</p>`)
-        }
-
-        if (entry.comment) {
-          // comm.push('      <p><i>Notes (not normative):</i></p><ul>')
-          comm.push(H`<p> ${entry.commentSource}: ${entry.comment}</p>`)
-        }
-      }
-      //comm.push('    </ul>')
-
-      if (comm.length) out.push('    <tr><td colspan="2">',
-                                '      <p><i>Notes (not normative):</i></p>',
-                                ...comm,
-                                '</td></tr>')
-
+  /*
+    const tags = {} // tags[deftext][tagname] = [src1link, src2link]
+    const comments = {} // comments[deftext] = [comment1, comment2, .. ]
+    for (const def of s.defs) {
+    def.signalTitle = s.title
+    allDefs.push(def)
+    if (!tags[def.text]) tags[def.text] = {}
+    for (const tagentry of def.tags || []) {
+    if (!tags[def.text][tagentry.name]) tags[def.text][tagentry.name] = []
+    tags[def.text][tagentry.name].push(tagentry.link)
+    // didStuff = true
     }
-    out.push('  <tbody>')
-    out.push('</table>')
-  } else {
-    // out.push('<p><i>No definitions found.</i></p>')
+    if (def.comments) {
+    if (!comments[def.text]) comments[def.text] = []
+    comments[def.text].push(def.comments)
+    }
+    }
+    // if (didStuff) console.error('TAGS', tags)
+    */
+  for (const defText of defTexts.values()) {
+    out.push('    <tr>')
+    const k = pkey.hash16(defText)
+    out.push(H`      <td id="${k}"><a href="#${k}" style="font-size: 75%">${k}<a></td>`)
+    out.push(H`      <td style="white-space: pre-line">${defText}</td>`)
+    /*
+      const tt = [] // same as "out", but we join with no space
+      for (const tagname of Object.keys(tags[text]).sort()) {
+      tt.push(H`${tagname}(`)
+      let links = tags[text][tagname]
+      if (links.length > 3) {
+      links = links.slice(0, 3).push('...')
+      }
+      tt.push(links.join(','))
+      tt.push(') ')
+      }
+    */
+    out.push('    </tr>')
+    
+    const comm = []
+
+    for (const entry of defs) {
+      if (entry.description !== defText) continue
+      
+      if (entry.source) {
+        comm.push(`<p><i>Expected data source:</i> ${entry.source}`)
+        if (entry.index && entry.id) {
+          comm.push(` via <a href="https://api.newsq.net/signals/${entry.index}/documentation">NewsQ (${entry.id}</a>)`)
+        }
+        comm.push(`</p>`)
+      }
+      
+      if (entry.demoOnly) {
+        comm.push(`<p><i>For Demonstration Purposes Only</i>.`)
+        comm.push(`</p>`)
+      }
+
+      if (entry.comment) {
+        comm.push(H`<p><i>Comment from</i> ${entry.commentSource}: ${entry.comment}</p>`)
+      }
+    }
+    //comm.push('    </ul>')
+
+    if (comm.length) out.push('    <tr><td colspan="2">',
+                              '      <p style="text-align: center"><i>Notes (not normative):</i></p>',
+                              ...comm,
+                              '</td></tr>')
+
   }
+  out.push('  <tbody>')
+  out.push('</table>')
   return out
 }
 
@@ -256,7 +253,7 @@ const schema = [
 
 const main = async () => {
   const newsqText = await fs.readFile('newsq2.ft', 'utf8')
-  // newsQ.push(...glue(newsqText, schema))
+  defs.push(...glue(newsqText, schema))
   // console.log({newsQ})
 
   defs.push(...glue(await fs.readFile('more.ft', 'utf8'), schema))
@@ -267,3 +264,9 @@ const main = async () => {
 }
 
 main()
+
+
+/// find all defs with that label
+
+/// find all defs with no label? or no label in the hierarchy?
+
